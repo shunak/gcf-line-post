58719f5220cfffb9b1e7227420035bc456021905
shunak <shuna966@gmail.com>
2022-12-22 15:35:53 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import base64, hashlib, hmac
+import logging
+import urllib.request,requests, json
+
+from flask import abort, jsonify
+
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage
+)
+
+def main(request):
+    channel_secret = os.environ.get('LINE_CHANNEL_SECRET')
+    channel_access_token = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
+
+    line_bot_api = LineBotApi(channel_access_token)
+           
+    url = requests.get('NEWS_JSON_URL')
+    json_text = url.text
+    contents = json.loads(json_text)
+    chat_text=""
+
+    for (i,content) in enumerate(contents):
+        chat_text += content["topic"] + "\n" + content["url"] + "\n"
+        articles = TextSendMessage(text=chat_text)
+
+    line_bot_api.broadcast(articles)
+
+    return jsonify({ 'message': 'ok'})
